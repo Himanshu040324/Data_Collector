d26b3661bda1b61d8862ecf90f85fc12863b9139
Himanshu <himanshu.040324@gmail.com>
2025-08-16 19:42:03 +0530

    Update App.jsx
    edit const api_url = import... line (6)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 async function requestToSend(data) {
   try {
-    const API_URL = import.meta.env.VITE_API_URL;
+    const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
     const res = await fetch(`${API_URL}/submit`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
