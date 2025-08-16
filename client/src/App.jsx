e0c12f73d7c41b4bd1065ad49881010f53885248
Himanshu <himanshu.040324@email.com>
2025-08-16 23:10:59 +0530

    Styling & Features commit 4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,117 +42,123 @@ function App() {
 
   return (
     <>
-      <div className=" bg-amber-200 text-xl m-2 sm:m-10 lg:text-3xl lg:min-w-[700px] p-4 rounded-4xl shadow-lg shadow-gray-500/50">
-        <div className=" flex justify-center items-center">
-          <div className=" my-[30px]">
-            <p className=" text-5xl  px-4 font-semibold w-fit">Form</p>
-            <div className=" min-h-[3px] bg-black rounded-full"></div>
+      <div className="flex flex-col min-h-[100%] m-2">
+        <div className=" bg-amber-200 text-xl m-2 sm:m-10 lg:text-3xl lg:min-w-[700px] p-4 rounded-4xl shadow-lg shadow-gray-500/50">
+          <div className=" flex justify-center items-center">
+            <div className=" my-[30px]">
+              <p className=" text-5xl  px-4 font-semibold w-fit">Form</p>
+              <div className=" min-h-[3px] bg-black rounded-full"></div>
+            </div>
           </div>
+
+          <form action="submit" onSubmit={handleClick}>
+            {/* first name */}
+            <div className=" flex flex-col sm:flex-row sm:justify-between justify-center items-start sm:items-center my-4">
+              <label htmlFor="fname" className=" m-2 p-1">
+                First Name
+              </label>
+
+              <input
+                id="fname"
+                name="fname"
+                type="text"
+                className=" border-2 mx-2 p-[0.3rem] rounded-sm bg-gray-100"
+                placeholder="Enter Your First Name"
+                required
+              />
+            </div>
+
+            {/* last name */}
+            <div className=" flex flex-col sm:flex-row sm:justify-between justify-center items-start sm:items-center my-4">
+              <label htmlFor="lname" className=" m-2 p-1">
+                Last Name
+              </label>
+
+              <input
+                id="lname"
+                name="lname"
+                type="text"
+                className=" border-2 mx-2 p-[0.3rem] rounded-sm bg-gray-100"
+                placeholder="Enter Your Last Name"
+                required
+              />
+            </div>
+
+            {/* age */}
+            <div className=" flex flex-col sm:flex-row sm:justify-between justify-center items-start sm:items-center my-4">
+              <label htmlFor="age" className=" m-2 p-1">
+                Age
+              </label>
+
+              <input
+                id="age"
+                name="age"
+                type="number"
+                className=" border-2 mx-2 p-[0.3rem] rounded-sm bg-gray-100"
+                placeholder="Enter Your Age"
+                required
+              />
+            </div>
+
+            {/* mail */}
+            <div className=" flex flex-col sm:flex-row sm:justify-between justify-center items-start sm:items-center my-4">
+              <label htmlFor="email" className=" m-2 p-1">
+                E-Mail
+              </label>
+
+              <input
+                id="email"
+                name="email"
+                type="email"
+                className=" border-2 mx-2 p-[0.3rem] rounded-sm bg-gray-100"
+                placeholder="email@example.com"
+                autoComplete="email"
+                required
+              />
+            </div>
+
+            {/* relation */}
+            <div className=" flex flex-col sm:flex-row sm:justify-between justify-center items-start sm:items-center my-4">
+              <label htmlFor="relation" className=" m-2 p-1">
+                Relation
+              </label>
+
+              <select
+                required
+                id="relation"
+                name="relation"
+                className="border-2 mx-2 p-[0.3rem] rounded-sm bg-gray-100"
+              >
+                <option value="">Select Relation</option>
+                <option value="friend">Friend</option>
+                <option value="brother">Brother</option>
+                <option value="sister">Sister</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+
+            {/* Submit button */}
+            <div className=" m-2 p-2]">
+              <button className=" m-5" type="submit" disabled={loading}>
+                {loading ? "Submitting..." : "Submit"}
+                {/* Button text changes based on loading state */}
+              </button>
+
+              <button
+                type="button" // important to prevent submitting the form
+                className="m-5 my-2 p-2"
+                onClick={(e) => e.currentTarget.closest("form").reset()}
+                disabled={loading}
+              >
+                {loading ? "Nope..." : "Reset"}
+              </button>
+            </div>
+          </form>
         </div>
-
-        <form action="submit" onSubmit={handleClick}>
-          {/* first name */}
-          <div className=" flex flex-col sm:flex-row sm:justify-between justify-center items-start sm:items-center my-4">
-            <label htmlFor="fname" className=" m-2 p-1">
-              First Name
-            </label>
-
-            <input
-              id="fname"
-              name="fname"
-              type="text"
-              className=" border-2 mx-2 p-[0.3rem] rounded-sm bg-gray-100"
-              placeholder="Enter Your First Name"
-              required
-            />
-          </div>
-
-          {/* last name */}
-          <div className=" flex flex-col sm:flex-row sm:justify-between justify-center items-start sm:items-center my-4">
-            <label htmlFor="lname" className=" m-2 p-1">
-              Last Name
-            </label>
-
-            <input
-              id="lname"
-              name="lname"
-              type="text"
-              className=" border-2 mx-2 p-[0.3rem] rounded-sm bg-gray-100"
-              placeholder="Enter Your Last Name"
-              required
-            />
-          </div>
-
-          {/* age */}
-          <div className=" flex flex-col sm:flex-row sm:justify-between justify-center items-start sm:items-center my-4">
-            <label htmlFor="age" className=" m-2 p-1">
-              Age
-            </label>
-
-            <input
-              id="age"
-              name="age"
-              type="number"
-              className=" border-2 mx-2 p-[0.3rem] rounded-sm bg-gray-100"
-              placeholder="Enter Your Age"
-              required
-            />
-          </div>
-
-          {/* mail */}
-          <div className=" flex flex-col sm:flex-row sm:justify-between justify-center items-start sm:items-center my-4">
-            <label htmlFor="email" className=" m-2 p-1">
-              E-Mail
-            </label>
-
-            <input
-              id="email"
-              name="email"
-              type="email"
-              className=" border-2 mx-2 p-[0.3rem] rounded-sm bg-gray-100"
-              placeholder="email@example.com"
-              autoComplete="email"
-              required
-            />
-          </div>
-
-          {/* relation */}
-          <div className=" flex flex-col sm:flex-row sm:justify-between justify-center items-start sm:items-center my-4">
-            <label htmlFor="relation" className=" m-2 p-1">
-              Relation
-            </label>
-
-            <select
-              required
-              id="relation"
-              name="relation"
-              className="border-2 mx-2 p-[0.3rem] rounded-sm bg-gray-100"
-            >
-              <option value="">Select Relation</option>
-              <option value="friend">Friend</option>
-              <option value="brother">Brother</option>
-              <option value="sister">Sister</option>
-              <option value="none">None</option>
-            </select>
-          </div>
-
-          {/* Submit button */}
-          <div className=" m-2 p-2">
-            <button className=" mx-5" type="submit" disabled={loading}>
-              {loading ? "Submitting..." : "Submit"}
-              {/* Button text changes based on loading state */}
-            </button>
-
-            <button
-              type="button" // important to prevent submitting the form
-              className="mx-5 my-2 p-2"
-              onClick={(e) => e.currentTarget.closest("form").reset()}
-              disabled={loading}
-            >
-              {loading ? "Nope..." : "Reset"}
-            </button>
-          </div>
-        </form>
+        {/* Footer */}
+        <footer className="bg-gray-800 text-white p-4 text-center mt-[6rem] text-2xl">
+          Made by Himanshu  ❣
+        </footer>
       </div>
     </>
   );
